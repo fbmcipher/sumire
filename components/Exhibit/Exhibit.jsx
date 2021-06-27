0c81fa6e282ae53cd3b8f4adc83f8a27adf32fe0
Faiz Saleem <hi@fzslm.me>
2021-06-27 03:04:41 +0100

    exhibit supports x-scroll

---
+++ components/Exhibit/Exhibit.jsx
@@ -0,0 +1,29 @@
+import styles from './Exhibit.module.css';
+import { useEffect, useRef } from 'react';
+
+export default function Exhibit({exhibit, children}){
+    /**
+     * This is the container element for an Exhibit.
+     */
+
+    /* this is equivalent of componentDidMount — i.e. only run once, when the
+       component is first rendered.
+       create an event handler to update the scroll bar component */
+
+    const exhibitContainerRef = useRef();
+
+    function onWheel(e){
+        /* map Y-scroll (most mice and trackpads) to X-scroll (most mice lack this) 
+           this works surprisingly well. it even supports inertial scrolling and all that jazz
+           for people with nice trackpads!   
+        */
+        exhibitContainerRef.current.scrollBy(e.deltaY, 0);
+    }
+
+    return (
+        <div ref={exhibitContainerRef} className={styles.exhibit_container} onWheel={onWheel}>
+            {children}
+        </div>
+    )
+
+}
