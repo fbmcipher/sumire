8c1fd6c908bbb730e3228a9248b7ab935672907b
Faiz Saleem <hi@fzslm.me>
2021-06-28 02:46:44 +0100

    seek kinda

--- components/NowPlaying/NowPlaying.jsx
+++ components/NowPlaying/NowPlaying.jsx
@@ -5,9 +5,36 @@ import styles from './NowPlaying.module.css';
 
 const NowPlaying = ({playing}) => {
 
+    return (
+        <PlaybackContext.Consumer>
+            { ({curTrack, audioTag}) => {
+                /* Instead of passing thru track duration as a prop to progress,
+                   I will manipulate the element directly using the traditional
+                   DOM methods to avoid crazy number of unnecessary renders. */
+                if (!curTrack) return null;
+
+                return (
+                    <div className={styles.nowPlaying}>
+                        <div className={styles.trackText}>
+                            <div className={styles.trackTitle}>{curTrack.title}</div>
+                            <Artists artists={curTrack.artists} />
+                        </div>
+                        <div className={styles.trackSeek}>
+                            <SeekBar audioTag={audioTag} />
+                        </div>
+                        <div className={styles.trackControls}>
+
+                        </div>
+                    </div>
+                )
+            } }
+        </PlaybackContext.Consumer>
+    )
+}
+
+const SeekBar = ({audioTag}) => {
     const seekBar = useRef();
     const [ didSetAudioTagListener, setDidSetAudioTagListener ] = useState(false);
-    const [ didSetSeekBarListener, setDidSetSeekBarListener ] = useState(false);
     const [ isSeeking, _setIsSeeking ] = useState(false);
 
     /* We can't access state inside an event listener in React in functional components.
@@ -22,72 +49,59 @@ const NowPlaying = ({playing}) => {
         _setIsSeeking(data);
     }
 
+    var seeking = false;
+
+    if(audioTag.current && seekBar.current && !didSetAudioTagListener){
+        audioTag.current.addEventListener('timeupdate', (e)=>{
+            /* keeps the slider synched with current playback completion */
+            /* playback percentage */
+            
+            /* don't update the range slider if user is seeking. this will
+            cause the range input to flicker between the value the user is
+            trying to input vs. the one we are updating */
+            if(isSeekingRef.current) return false;
+
+            let perc = (audioTag.current.currentTime / audioTag.current.duration);
+            /* set seekBar progress attribute */
+            seekBar.current.value = perc * 100;
+        });
+
+        /* ensure we only set this listener once */
+        setDidSetAudioTagListener(true);
+    }
+
+    const onChange = e => {
+        /* calculate the time to seek to from the percentage */
+        let newPerc = e.target.value / 100;
+        let newTime = audioTag.current.duration * newPerc;
+        seekBar.current.value = e.target.value;
+        audioTag.current.currentTime = newTime;
+    }
+
+    console.log('rerender')
+
+    const onMouseDown = e => {
+        /* when seekbar is receiving mouse/touch input, set isSeeking
+           so timeupdate handler stops updating the input while
+           the user is trying to interact with it. 
+        */ 
+        setIsSeeking(true);
+    }
+
+    const onMouseUp = e => {
+        setIsSeeking(false);
+    }
+
     return (
-        <PlaybackContext.Consumer>
-            { ({curTrack, audioTag}) => {
-                /* Instead of passing thru track duration as a prop to progress,
-                   I will manipulate the element directly using the traditional
-                   DOM methods to avoid crazy number of unnecessary renders. */
-                if (!curTrack) return null;
-
-                if(audioTag.current && !didSetAudioTagListener){
-                    audioTag.current.addEventListener('timeupdate', (e)=>{
-                        /* keeps the slider synched with current playback completion */
-                        /* playback percentage */
-                        
-                        /* don't update the range slider if user is seeking. this will
-                        cause the range input to flicker between the value the user is
-                        trying to input vs. the one we are updating */
-                        if(isSeekingRef.current) return false;
-
-                        let perc = (audioTag.current.currentTime / audioTag.current.duration);
-                        /* set seekBar progress attribute */
-                        seekBar.current.value = perc * 100;
-                    });
-
-                    /* ensure we only set this listener once */
-                    setDidSetAudioTagListener(true);
-                }
-
-                if(seekBar.current && !didSetSeekBarListener){
-                    seekBar.current.addEventListener('change', (e)=>{
-                        /* calculate the time to seek to from the percentage */
-                        let newPerc = e.target.value / 100;
-                        let newTime = audioTag.current.duration * newPerc;
-                        audioTag.current.currentTime = newTime;
-                    })
-
-                    seekBar.current.addEventListener('mousedown', (e)=>{
-                        /* when seekbar is receiving mouse/touch input, set isSeeking
-                            so timeupdate handler stops updating the input while
-                            the user is trying to interact with it. */ 
-                        setIsSeeking(true);
-                    })
-
-                    seekBar.current.addEventListener('mouseup', (e)=>{
-                        setIsSeeking(false);
-                    })
-
-                    /* ensure we only set this listener once */
-                    setDidSetSeekBarListener(true);
-                }
-
-                return (
-                    <div className={styles.nowPlaying}>
-                        <div className={styles.trackText}>
-                            <div className={styles.trackTitle}>{curTrack.title}</div>
-                            <Artists artists={curTrack.artists} />
-                        </div>
-                        <div className={styles.trackSeek}>
-                            <input min="0" max="100" value="0" type="range" ref={seekBar} />
-                        </div>
-                        <div className={styles.trackControls}>
-
-                        </div>
-                    </div>
-                )
-            } }
-        </PlaybackContext.Consumer>
+        <div className={styles.trackSeek}>
+            <input 
+                onChange={onChange}
+                onMouseDown={onMouseDown}
+                onTouchStart={onMouseDown}
+                onMouseUp={onMouseUp}
+                onTouchEnd={onMouseUp}
+            min="0" max="100" value={0.5} type="range" ref={seekBar} />
+        </div>
     )
 }
 
