0bb3045d2c7dbf52f8ec9ef541145c9aa6afd7c3
Faiz Saleem <hi@fzslm.me>
2021-07-03 05:45:02 +0100

    scroll wheel works on home page now

--- components/Carousel/Carousel.jsx
+++ components/Carousel/Carousel.jsx
@@ -2,12 +2,24 @@
  *  between its children.
  */
 
-import React, { useState }  from 'react';
+import React, { useRef }  from 'react';
 import styles from './Carousel.module.css';
 
 const Carousel = ({children}) => {
+
+    const carouselContainerRef = useRef();
+
+    function onWheel(e){
+        /* map Y-scroll (most mice and trackpads) to X-scroll (most mice lack this) 
+           this works surprisingly well. it even supports inertial scrolling and all that jazz
+           for people with nice trackpads!   
+        */
+        console.log(e.deltaY);
+        carouselContainerRef.current.scrollBy(e.deltaY, 0);
+    }
+
     return (
-        <div className={styles.carouselContainer}>
+        <div onWheel={onWheel} ref={carouselContainerRef} className={styles.carouselContainer}>
             <div class={`${styles.carousel} carousel`}>
                 {children}
             </div>
