3dc66ad8f7828cccf98582f376b37f211b7920a1
Faiz Saleem <hi@fzslm.me>
2021-07-12 03:25:05 +0100

    about page done

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,21 @@
-export default function About(){
+import styles from './about.module.css';
+import { Favorite } from '@material-ui/icons'
+
+const About = () => {
     return (
-        <main className="flex-center">
-            <h1>404</h1>
-            <p>this page doesn't (or never did) exist...</p>
+        <main className={styles.about}>
+            <div className={styles.content}>
+                <p>
+                    <span className={styles.sumireName}>sumire,</span> named for the Japanese perennial flower, is an art house active in creative hotspots around the world - Leeds, London, Sheffield, New York, and more.
+                </p>
+                <p>we don’t restrict ourselves to a particular medium or kind of art. we prioritise authenticity, and allowing each artist to speak on what touches them, beyond all else.</p>
+                <p>we honour independent art and creativity of all kinds — musicians, graphic designers, filmmakers, visual artists, developers, and more.</p>
+                <p>we feel that the priorities of the industry and artists are out of line - more than ever in our modern age. we don’t enforce bureaucracy or structure on our artists. we are all equal in our common goal of empowering authentic and encourage creative expression.</p>
+                <p>if you want to get in touch with us about a creative opportunities, our socials are always open!</p>
+                <Favorite />
+            </div>
         </main>
     )
 }
+
+export default About
