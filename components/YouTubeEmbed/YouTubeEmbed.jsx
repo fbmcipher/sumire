5673b12b2560638830996de447da70a3741e602d
Faiz Saleem <hi@fzslm.me>
2021-08-06 05:15:09 +0100

    HAIDERNISM YTEMBED NOT RESEMBLING SHIT

--- components/YouTubeEmbed/YouTubeEmbed.jsx
+++ components/YouTubeEmbed/YouTubeEmbed.jsx
@@ -1,10 +1,7 @@
 import styles from './YouTubeEmbed.module.css';
 import { useMediaQuery } from 'react-responsive';
-import { useWindowWidth } from '@react-hook/window-size';
 
-const YouTubeEmbed = ({ytid, showControls = true, height, width, layout = false}) => {
-    const windowWidth = useWindowWidth();
-
+const YouTubeEmbed = ({ytid, showControls = false, height, width, layout = false}) => {
     if(layout && layout == 'full'){
         /* Fill the entire screen. */
         const isVertical = useMediaQuery({query: '(max-width: 1000px)'});
@@ -13,11 +10,8 @@ const YouTubeEmbed = ({ytid, showControls = true, height, width, layout = false}
             width = '100vw';
             height = 'unset';
         } else {
-
-            /* Get the aspect ratio of the display, and multiply it by the height of the window. */
-
+            width = '171vh';
             height = '100vh';
-            width = (windowWidth || document.documentElement.clientWidth) + 'px';
         }
     }
 
@@ -27,7 +21,7 @@ const YouTubeEmbed = ({ytid, showControls = true, height, width, layout = false}
             width
         }}>
             <div className={styles.frameContainer}>
-                <iframe width="858" height="483" src={`https://www.youtube.com/embed/${ytid}?controls=0&autoplay=1&loop=1&modsetbranding&showinfo=0&playlist=${ytid}&mute=1`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                <iframe width="858" height="483" src={`https://www.youtube.com/embed/${ytid}?controls=${showControls ? 1 : 0}&autoplay=1&loop=1&modsetbranding&showinfo=0&playlist=${ytid}&mute=1`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
         </div>
     )
